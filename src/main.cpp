0449c2046673cd4500b6461a5b6e79f9ae0c5102
Your Name <you@example.com>
2020-10-05 16:58:15 +0100

    Its now Luigi

--- src/main.cpp
+++ src/main.cpp
@@ -38,7 +38,7 @@ void on_cleanup()
 
 int main(int argc, char* args[])
 {
-	util::sdl_initialize("Hello Again", 800, 600);
+	util::sdl_initialize("Title Change", 800, 600);
 
 	on_load();
 	SDL_SetWindowIcon(util::sdl_window, icon);
